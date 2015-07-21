59675dcf3dd7ea03ab4c1da25f77f6d72a61eff4
Brandon Graves <bgraves14@gmail.com>
2015-07-21 18:40:37 +0100

    Post challenge 1

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,19 @@ require 'BikeClass.rb'
 describe DockingStation do
 	it{is_expected.to respond_to(:release_bike)}
 
-	it "responds to bike.working?" do
+	it "releases working bikes" do
+		subject.dock Bike.new
 		bike = subject.release_bike
 		expect(bike).to be_working
 	end
+	it { is_expected.to respond_to(:dock).with(1).argument}
+
+	describe '#release_bike' do
+		it 'raises an error when there are no bikes available' do
+			expect { subject.release_bike }.to raise_error 'No bikes available'
+		end
+	end
+
 end
+
 
